2001fdea81f77ccf459ff0653132a9efe41c57f5
Luciano Andrade <lunodrade@gmail.com>
2019-06-04 15:34:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.debug = process.env.DEBUG || false;
 
 config.mqtt  = {};
 config.mqtt.namespace = process.env.MQTT_NAMESPACE || '/MIGRA/#';
-config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || '127.0.0.1';
+config.mqtt.hostname  = process.env.MQTT_HOSTNAME  || 'broker.hivemq.com';
 config.mqtt.port      = process.env.MQTT_PORT      || 1883;
 
 config.mongodb = {};
